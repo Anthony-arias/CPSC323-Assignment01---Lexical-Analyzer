f5c256faaa9aaf45b9bef20a9b9eff0e9116c30b
Anthony Arias <joel246y@gmail.com>
2023-04-06 22:41:21 -0700

    Added rules 25 to 37

--- CPSC323 Assignment01 - Lexical Analyzer/SyntaxAnalyzer.h
+++ CPSC323 Assignment01 - Lexical Analyzer/SyntaxAnalyzer.h
@@ -6,6 +6,9 @@ class SyntaxAnalyzer {
 public:
 
     vector<Token> syntaxTokens;
+    //helper functions
+    void outputTokenValueAndIterate();
+    void throwError();
 
     // Rule 1
     bool rat23S();
@@ -17,6 +20,32 @@ public:
     bool function();
     bool functionDefinitionsPrime();
 
+
+
+
+
+
+
+
+
+
+
+
+
+    // Rule 25 - 37
+    void returnRule();
+    void returnRulePrime();
+    void print();
+    void scan();
+    void whileRule();
+    void condition();
+    void relop();
+    void expression();
+    void expressionPrime();
+    void term();
+    void termPrime();
+    void factor();
+    void primary();
 private:
     LexerAnalyzer lexer;
     int current_token_index;
